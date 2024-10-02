3546b9de629096c2c84e5a8575f1a318ed667df7
mbarnesdev <martinbarnesweb@gmail.com>
2024-10-02 14:00:05 +0100

    feat: config for custom domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,5 @@ import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
   integrations: [tailwind()],
-  site: "https://mbarnesdev.github.io",
-  base: "martinbarnes.dev"
+  site: "https://martinbarnes.dev",
 });
